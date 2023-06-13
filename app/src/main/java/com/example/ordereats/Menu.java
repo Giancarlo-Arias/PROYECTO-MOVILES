378b998e94c526e7124acd258ece5238d7b02ac0
Giancarlo Arias <giancarlo.arias.paisano@est.una.ac.cr>
2023-06-13 15:12:29 -0600

    .
    .

--- app/src/main/java/com/example/ordereats/Menu.java
+++ app/src/main/java/com/example/ordereats/Menu.java
@@ -12,7 +12,8 @@ public class Menu extends AppCompatActivity {
 
     private TextView txtWelcome;
 
-    Button logout;
+    Button logout, listaUsuario;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +33,15 @@ public class Menu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Menu.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        listaUsuario= (Button) findViewById(R.id.btnListUs);
+        listaUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Menu.this, ListarUsuarioActivity.class);
                 startActivity(i);
             }
         });
