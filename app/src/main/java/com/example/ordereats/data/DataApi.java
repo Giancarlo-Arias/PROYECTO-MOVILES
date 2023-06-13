7f5b63ae2159b99cf87706b693f23405aa7911bd
Yairu4439 <97989212+Yairu4439@users.noreply.github.com>
2023-06-12 22:03:08 -0600

    ttt

--- app/src/main/java/com/example/ordereats/data/DataApi.java
+++ app/src/main/java/com/example/ordereats/data/DataApi.java
@@ -1,28 +1,28 @@
 package com.example.ordereats.data;
 
 public class DataApi {
-    public String ip="http://192.168.100.37/enlaceApiConeccionDBordereats/";
+    public static String ip="http://192.168.100.37/enlaceApiConeccionDBordereats/";
 
     /*API platillos*/
-    public String insertarPlatillo=ip+"PlatoApi.php?accion=InsertarPlatillo";
-    public String obtenerPlatoPorId=ip+"PlatoApi.php?accion=ObtenerPlatoPorId";
-    public String obtenerPlatillos=ip+"PlatoApi.php?accion=ObtenerPlatillos";
-    public String eliminarPlatoPorId=ip+"PlatoApi.php?accion=EliminarPlatoPorId";
-    public String actualizarPlato=ip+"PlatoApi.php?accion=ActualizarPlato";
+    public static String insertarPlatillo=ip+"PlatoApi.php?accion=InsertarPlatillo";
+    public static String obtenerPlatoPorId=ip+"PlatoApi.php?accion=ObtenerPlatoPorId";
+    public static String obtenerPlatillos=ip+"PlatoApi.php?accion=ObtenerPlatillos";
+    public static String eliminarPlatoPorId=ip+"PlatoApi.php?accion=EliminarPlatoPorId";
+    public static String actualizarPlato=ip+"PlatoApi.php?accion=ActualizarPlato";
 
     /*API Guarniciones */
-    public String insertarGuarnicion=ip+"GuarnicionesApi.php?accion=InsertarGuarnicion";
-    public String obtenerTodosGuarnicion=ip+"GuarnicionesApi.php?accion=ObtenerTodosGuarnicion";
-    public String eliminarGuarnicionPorId=ip+"GuarnicionesApi.php?accion=EliminarGuarnicionPorId";
-    public String obtenerGuarnicionPorId=ip+"GuarnicionesApi.php?accion=ObtenerGuarnicionPorId";
-    public String actualizarGuarnicion=ip+"GuarnicionesApi.php?accion=ActualizarGuarnicion";
+    public static String insertarGuarnicion=ip+"GuarnicionesApi.php?accion=InsertarGuarnicion";
+    public static String obtenerTodosGuarnicion=ip+"GuarnicionesApi.php?accion=ObtenerTodosGuarnicion";
+    public static String eliminarGuarnicionPorId=ip+"GuarnicionesApi.php?accion=EliminarGuarnicionPorId";
+    public static String obtenerGuarnicionPorId=ip+"GuarnicionesApi.php?accion=ObtenerGuarnicionPorId";
+    public static String actualizarGuarnicion=ip+"GuarnicionesApi.php?accion=ActualizarGuarnicion";
 
     /*API Ingredientes especiales*/
-    public String insertarIngredienteEspecial=ip+"IngredienteEspecialApi.php?accion=InsertarIngredienteEspecial";
-    public String obtenerTodosIngredientesEspeciales=ip+"IngredienteEspecialApi.php?accion=ObtenerTodosIngredientesEspeciales";
-    public String eliminarIngredienteEspecial=ip+"IngredienteEspecialApi.php?accion=EliminarIngredienteEspecial";
-    public String actualizarIngredienteEspecial=ip+"IngredienteEspecialApi.php?accion=ActualizarIngredienteEspecial";
-    public String obtenerIngredienteEspecialPorId=ip+"IngredienteEspecialApi.php?accion=ObtenerIngredienteEspecialPorId";
+    public static String insertarIngredienteEspecial=ip+"IngredienteEspecialApi.php?accion=InsertarIngredienteEspecial";
+    public static String obtenerTodosIngredientesEspeciales=ip+"IngredienteEspecialApi.php?accion=ObtenerTodosIngredientesEspeciales";
+    public static String eliminarIngredienteEspecial=ip+"IngredienteEspecialApi.php?accion=EliminarIngredienteEspecial";
+    public static String actualizarIngredienteEspecial=ip+"IngredienteEspecialApi.php?accion=ActualizarIngredienteEspecial";
+    public static String obtenerIngredienteEspecialPorId=ip+"IngredienteEspecialApi.php?accion=ObtenerIngredienteEspecialPorId";
 
 
 }
